827a1d271e393746de87d4b4682f0d72cf0b53de
abdel-concordia <abdel@MSSPro.wireless.concordia.ca>
2024-01-26 11:37:57 -0500

    Little bit of pass by value and pass by reference

--- COMP249_U/src/comp249_u/Driver.java
+++ COMP249_U/src/comp249_u/Driver.java
@@ -5,10 +5,16 @@ public class Driver {
     public static void main(String[] args) {
 
         Person p1 = new Person("Peter", 19, "Montreal");
-        Person p2 = p1;
 
+        System.out.println(p1);
         System.out.println(p1);
 
     }
 
+    public void someMethod(Person p) {
+        //p.setName("New name");
+
+        p = new Person("BlaBlah", 5, "Laval");
+
+    }
 }
