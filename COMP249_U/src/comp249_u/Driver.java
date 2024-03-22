616508ca3b72f210e7f75ad1475258557bd3ac20
abdel-concordia <abdel@MSSPro.wireless.concordia.ca>
2024-03-22 11:26:11 -0400

    Inner classes example

--- COMP249_U/src/comp249_u/Driver.java
+++ COMP249_U/src/comp249_u/Driver.java
@@ -4,79 +4,33 @@ public class Driver {
 
     public static void main(String[] args) {
 
-        A a = new A(20);
-        A x = new A(20);
-
-        System.out.println(a.compareTo(x));
-
-        System.out.println("value in a " + a.getNumber());
-
-        try {
-            A b = (A) a.clone();
-            System.out.println("value in b " + a.getNumber());
-
-        } catch (CloneNotSupportedException cnfex) {
-            System.out.println("Cannot clone " + cnfex.getMessage());
-        }
     }
 
 }
 
-class A implements Cloneable, Comparable {
+class A {
 
-    private B b;
-    private C c;
     private int n;
+    private String s;
 
-    public A(int n) {
-        b = new B();
-        c = new C();
-        this.n = n;
+    B b;
+
+    public void f() {
+        B b = new B();
+        b.number = 7;
     }
 
-    public int getNumber() {
-        return this.n;
-    }
+    public class B { // inner class
 
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        A copy = (A) super.clone();
-        copy.b = (B) b.clone();
-        copy.c = (C) c.clone();
-        copy.n = n;
+        private int number;
 
-        return copy;
-    }
+        public void g() {
+            int x = n * 2;
+        }
 
-    @Override
-    public int compareTo(Object otherObject) {
+        public class C {
 
-        A otherA = (A) otherObject;
-
-        if (this.n < otherA.n) {
-            return -1;
-        } else if (this.n == otherA.n) {
-            return 0;
-        } else {
-            return 1;
         }
     }
-}
 
-class B implements Cloneable {
-
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        B copy = (B) super.clone();
-        return copy;
-    }
-}
-
-class C implements Cloneable {
-
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        C copy = (C) super.clone();
-        return copy;
-    }
 }
