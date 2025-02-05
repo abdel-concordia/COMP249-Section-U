2aec7352c4ce916d58e65e242189151526541e86
Abdel MSSPRO <abdel@MSSPRO>
2025-02-05 11:24:12 -0500

    static members and inheritance example

--- COMP249_Section_U/src/comp249_section_u/DriverClass.java
+++ COMP249_Section_U/src/comp249_section_u/DriverClass.java
@@ -1,18 +1,47 @@
 package comp249_section_u;
 
-import java.util.Scanner;
-
 public class DriverClass {
 
     public static void main(String[] args) {
 
-        Address ad1 = new Address(300, "Sherbrook West", "Montreal");
-        Student s1 = new Student("John", 10, ad1, "123", 0.0);
+//        Address ad1 = new Address(300, "Sherbrook West", "Montreal");
+//        Student s1 = new Student("John", 10, ad1, "123", 0.0);
+//
+//        Person p1 = new Person("ALi", 7, ad1);
+//
+//        System.out.println(p1);
+        A a = new A();
+        System.out.println(a.x);
 
-        Person p1 = new Person("ALi", 7, ad1);
+        B b = new B();
+        System.out.println(b.x);
 
-        System.out.println(p1);
+        System.out.println(a.x);
 
     }
+}
 
+class A {
+
+    static int x;
+
+    public A() {
+        x++;
+    }
+
+    public static void f() {
+
+    }
+}
+
+class B extends A {
+
+    public B() {
+        x++;
+    }
+
+    @Override
+    public static void f() {
+
+    }
 }
