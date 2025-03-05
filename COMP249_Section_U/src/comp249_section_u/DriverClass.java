ccaab247744ec5ceea2c59474c5b1be60c6da032
Abdel MSSPRO <abdel@MSSPRO>
2025-03-05 11:22:45 -0500

    Writing/reading binary files

--- COMP249_Section_U/src/comp249_section_u/DriverClass.java
+++ COMP249_Section_U/src/comp249_section_u/DriverClass.java
@@ -1,46 +1,77 @@
 package comp249_section_u;
 
+import java.io.EOFException;
 import java.io.FileInputStream;
-import java.io.PrintWriter;
-import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class DriverClass {
 
     public static void main(String[] args) {
 
         /*
-        // Create the PrintWriter stream
-        PrintWriter output = null;
+        // Writing to a binary file
+        // Open the file
+        ObjectOutputStream oos = null;
         try {
-            output = new PrintWriter(new FileOutputStream("myfile.txt"));
+            oos = new ObjectOutputStream(new FileOutputStream("data.bin"));
         } catch (FileNotFoundException fnfex) {
-            System.out.println("Cannot open the file for writing.");
+            System.out.println("Cannot open/create the file.");
+            System.exit(0);
+        } catch (IOException ioex) {
+            System.out.println("Cannot open the file");
             System.exit(0);
         }
-        // Write
-        output.print("Hello");
-        output.println(" World");
-        output.print("Hi");
-        // Close
-        output.close();
-         */
-        // Create the Scanner
-        Scanner sc = null;
+        // Write to the file
+        int a = 5, b = 10, c = 20;
         try {
-            sc = new Scanner(new FileInputStream("myfile.txt"));
-        } catch (FileNotFoundException fnfex) {
-            System.out.println("Cannot open the input file.");
+            oos.writeInt(a);
+            oos.writeInt(b);
+            oos.writeInt(c);
+        } catch (IOException ioex) {
+            System.out.println("Cannot read from file.");
+            System.exit(0);
         }
-        // Read
-        while (sc.hasNext()) {
-            String s = sc.nextLine();
-            System.out.println(s);
+        // Close the file
+        try {
+            oos.close();
+        } catch (IOException ioex) {
+            System.out.println("Cannot close the file.");
         }
-        // Close
-        sc.close();
 
+         */
+        // Reading from a binary
+        // Open the file for input
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(new FileInputStream("data.bin"));
+        } catch (FileNotFoundException fnfex) {
+            System.out.println("Cannot open the file");
+        } catch (IOException ioex) {
+            System.out.println("Cannot open the file");
+        }
+        // Read from file
+        while (true) {
+            try {
+                int num = ois.readInt();
+                System.out.println(num);
+            } catch (EOFException eofex) {
+                break;
+            } catch (IOException ioex) {
+                System.out.println("Cannot read from file.");
+                break;
+            }
+        }
+
+        // Close the file
+        try {
+            ois.close();
+        } catch (IOException ioex) {
+            System.out.println("Cannot close the file.");
+        }
     }
 
 }
