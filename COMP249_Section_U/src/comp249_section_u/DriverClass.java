305c9165b07af3f4ade84af4d2d6e4ca31281788
Abdel MSSPRO <abdel@MSSPRO>
2025-03-12 11:05:18 -0400

    Recursion examples

--- COMP249_Section_U/src/comp249_section_u/DriverClass.java
+++ COMP249_Section_U/src/comp249_section_u/DriverClass.java
@@ -4,7 +4,11 @@ public class DriverClass {
 
     public static void main(String[] args) {
 
-        System.out.println(sumDown(-4));
+        DriverClass obj = new DriverClass();
+
+        int[] arr = {-1, 2, 3, -4};
+
+        System.out.println(obj.sumPositive(arr, arr.length - 1));
     }
 
     public static void countDown(int n) { // Assumption: n is positive
@@ -40,6 +44,47 @@ public class DriverClass {
     }
 
     public static int sumArray(int[] numbers) {
+// Base case
+        if (numbers.length == 1) {
+            return numbers[0];
+        } else {// Recursive call
+            // create a smaller array with remaining values
+            int[] arr = new int[numbers.length - 1];
+            // Copy remaining elements to the new array
+            for (int i = 1; i < numbers.length; i++) {
+                arr[i - 1] = numbers[i];
+            }
+            // Call the method on that array
+            return numbers[0] + sumArray(arr);
+        }
+    }
 
+    public static int sumArray(int[] numbers, int index) {
+        // Base case
+        if (index == numbers.length - 1) {
+            return numbers[index];
+        } else { // Recursive call
+            return numbers[index] + sumArray(numbers, index + 1);
+        }
+    }
+
+    public static int countPositive(int[] numbers, int index) {
+        // Base case
+        if (index == 0) {
+            return numbers[0] < 0 ? 0 : 1;
+        } else {
+            // Recursive call
+            return numbers[index] < 0 ? countPositive(numbers, --index) : 1 + countPositive(numbers, --index);
+        }
+    }
+
+    public int sumPositive(int[] numbers, int index) {
+        // Base case
+        if (index == 0) {
+            return numbers[0] < 0 ? 0 : numbers[0];
+        } else {
+            // Recursive call
+            return numbers[index] < 0 ? sumPositive(numbers, --index) : numbers[index] + sumPositive(numbers, --index);
+        }
     }
 }
