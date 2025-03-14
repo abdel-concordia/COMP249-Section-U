679ac3364ae918d7909b79e2154374455bc1ed7d
Abdel MSSPRO <abdel@MSSPRO>
2025-03-14 11:21:06 -0400

    Illustrating difference between Copy Constructor and clone method

--- COMP249_Section_U/src/comp249_section_u/DriverClass.java
+++ COMP249_Section_U/src/comp249_section_u/DriverClass.java
@@ -4,52 +4,83 @@ public class DriverClass {
 
     public static void main(String[] args) {
 
-        A o1 = new A();
-        MyInterface o2 = new A();
-        MyInterface o3 = new B();
+        X x1 = new X();
+        X x2 = new X(x1);
 
-        MyInterface[] objs = new MyInterface[4];
-        objs[0] = o1;
-        objs[1] = o2;
-        objs[2] = o3;
-        objs[3] = new C();
+        Y y1 = new Y();
+        Y y2 = new Y(y1);
+
+        Z z1 = new Z();
+
+        new X(x1); // creates an object of X
+        new X(y1); // creates an object of X
+        new X(z1); // creates an object of X
+
+        x1.clone(); // creates an object of X
+        y1.clone(); // creates an object of Y
+        z1.clone(); // creates an object of Z
+
+        X x3 = z1;
+
+        x3.clone(); // Creates an object of Z
+
+        yn.clone(); // create an object of ???
+    }
+
+}
+
+class X {
+
+    public X() {
 
     }
 
-    public static void g(MyInterface obj) {
+    public X(X o) {
 
     }
 }
 
-class A implements MyInterface {
+class Y extends X {
 
-    @Override
-    public void f1() {
-        // something
-    }
-
-    public void f5() {
+    public Y() {
 
     }
-}
 
-class B implements MyInterface {
-
-    @Override
-    public void f1() {
-        // something
-    }
-
-    public void f6() {
+    public Y(Y o) {
 
     }
 }
 
-class C implements MyInterface {
+class Z extends Y {
+
+}
+
+class A implements Cloneable {
+
+    int x; // primitive type
 
     @Override
-    public void f1() {
-        // something
+    public Object clone() throws CloneNotSupportedException {
+        A cp = (A) super.clone();
+        cp.x = this.x;
+        return cp;
     }
+}
 
+class C implements Cloneable {
+
+    int z; // primitive type
+    A obj; // Reference type, mutable
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        // Clone with super
+        C cp = (C) super.clone();
+        // Clone what the class added
+        cp.z = this.z;
+        cp.obj = (A) this.obj.clone();
+
+        // return the created object
+        return cp;
+    }
 }
