c3b0bd8037359ad89bc2b9cd8f9a31d4be730e9c
Abdel MSSPRO <abdel@MSSPRO>
2025-03-19 11:30:43 -0400

    ArrayList example 1

--- COMP249_Section_U/src/comp249_section_u/DriverClass.java
+++ COMP249_Section_U/src/comp249_section_u/DriverClass.java
@@ -1,31 +1,17 @@
 package comp249_section_u;
 
+import java.util.ArrayList;
+
 public class DriverClass {
 
     public static void main(String[] args) {
 
-        MyInterface obj1 = new MyInterface() { // object out of an anonymous class
+        ArrayList<String> strings = new ArrayList<>(5); // 5: initial capacity
 
-            @Override
-            public void f() {
+        strings.add("Hello");
 
-            }
-        };
-
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
     }
 
 }
-
-interface MyInterface {
-
-    void f();
-}
-
-class A implements I { // named class
-
-}
-
-// Interfaces can be implemented in 3 ways:
-// 1. Named class
-// 2. ANonymous class
-// 3. Lambda expression (search for lambda expression and functional interfaces)
