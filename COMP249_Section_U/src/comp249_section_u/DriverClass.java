ad03d65aba79dc91e3a8c73048afac8b282f789b
Abdel MSSPRO <abdel@MSSPRO>
2025-03-21 11:06:55 -0400

    ArrayList example 2

--- COMP249_Section_U/src/comp249_section_u/DriverClass.java
+++ COMP249_Section_U/src/comp249_section_u/DriverClass.java
@@ -1,17 +1,59 @@
 package comp249_section_u;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class DriverClass {
 
     public static void main(String[] args) {
 
-        ArrayList<String> strings = new ArrayList<>(5); // 5: initial capacity
+        ArrayList<A> objects = new ArrayList<>(5); // 5: initial capacity, not the LIMIT
 
-        strings.add("Hello");
+        objects.add(new A(5)); // appends
+        objects.add(new A(2)); // inserts the object at the specified position
 
-        ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(5);
+        System.out.println("Before sorting");
+        for (A s : objects) {
+            System.out.println(s);
+        }
+
+        Collections.sort(objects); // How to sort A objects
+
+        System.out.println("After sorting");
+
+        for (A s : objects) {
+            System.out.println(s);
+        }
+
+        //System.out.println(strings);
     }
 
 }
+
+class A implements Comparable {
+
+    private int x;
+    private int y;
+
+    public A(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public int compareTo(Object otherObject) {
+
+        A otherA = (A) otherObject; // Be carefull (if cast is not possible, this will throw an exception)
+
+        if (this.x < otherA.x) {
+            return -1;
+        } else if (this.x > otherA.x) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+}
+
+
+// If you need multiple ways to compare in the spame program, use the Comparator interface (compare)
