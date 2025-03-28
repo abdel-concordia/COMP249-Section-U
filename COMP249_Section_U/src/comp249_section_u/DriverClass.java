a9a2d80055e12987af880436f94e047867977a2c
Abdel MSSPRO <abdel@MSSPRO>
2025-03-28 11:24:41 -0400

    LinkedList example 2

--- COMP249_Section_U/src/comp249_section_u/DriverClass.java
+++ COMP249_Section_U/src/comp249_section_u/DriverClass.java
@@ -6,12 +6,13 @@ public class DriverClass {
 
         LinkedList myList = new LinkedList();
 
-        myList.addAtTail("1");
-        myList.addAtTail("2");
-        myList.addAtTail("3");
-
-        String value = myList.removeFirst();
-        System.out.println(value);
+        //myList.addAtTail("1");
+        //myList.addAtTail("2");
+        //myList.addAtTail("3");
+        //String value = myList.removeFirst();
+        //System.out.println(value);
+        myList.addAfter("25", "10");
+        //myList.addAtTail("7");
 
         myList.display();
     }
